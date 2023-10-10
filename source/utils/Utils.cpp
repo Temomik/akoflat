1c070db86a3a047fe2e0e5c7fecc1a982cb7e8a1
temomik <temomik44@gmail.com>
2023-10-10 12:24:14 +0300

    Added utils for IC string comapring

---
+++ source/utils/Utils.cpp
@@ -0,0 +1,47 @@
+#include "Utils.h"
+
+#include <cctype>
+#include <algorithm>
+
+namespace
+{
+    bool ICharEquals(char a, char b)
+    {
+        return std::toupper(static_cast<unsigned char>(a)) ==
+               std::toupper(static_cast<unsigned char>(b));
+    }
+}
+
+bool Utils::EqualsIC(const std::string_view &lhs, const std::string_view &rhs, const size_t length)
+{
+    size_t minLength = std::min<size_t>(lhs.length(), rhs.length());
+    auto lengthConstrain = std::clamp<size_t>(length, 0, minLength);
+    return std::equal(lhs.begin(), lhs.begin() + lengthConstrain, rhs.begin(),
+                      rhs.begin() + lengthConstrain, ICharEquals);
+}
+
+size_t Utils::FindIC(const std::string_view &source, const std::string_view &target, const size_t length, const size_t offset)
+{
+    if (offset + length > source.length())
+    {
+        return std::string_view::npos;
+    }
+
+    const char* sourceBegin = source.begin() + offset;
+    const char* sourceEnd= sourceBegin + length;
+
+    auto it = std::search(
+        sourceBegin, sourceEnd,
+        target.begin(), target.end(),
+        [](unsigned char lhs, unsigned char rhs)
+        { return std::toupper(lhs) == std::toupper(rhs); });
+
+    if (it == sourceEnd)
+    {
+        return std::string_view::npos;
+    }
+    else
+    {
+        return (it - source.begin()) / sizeof(char);
+    }
+}
---
+++ source/utils/Utils.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string_view>
+
+namespace Utils
+{
+    bool EqualsIC(const std::string_view& lhs, const std::string_view& rhs, const size_t length);
+    size_t FindIC(const std::string_view & source, const std::string_view & target, const size_t length,  const size_t offset = 0);
+}
